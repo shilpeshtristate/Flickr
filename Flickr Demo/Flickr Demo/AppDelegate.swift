96cc4a8b8b581e110893d96a70057e4dcd4f3a21
Shilpesh Shah <Shilpesh.shah@tristatetechnology.co.in>
2020-12-11 15:14:38 +0530

    Project Setup

---
+++ Flickr Demo/Flickr Demo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Flickr Demo
+//
+//  Created by Tristate Technology on 11/12/20.
+//  Copyright © 2020 Tristate Technology. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Flickr Demo/Flickr Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flickr Demo
+//
+//  Created by Tristate Technology on 11/12/20.
+//  Copyright © 2020 Tristate Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
